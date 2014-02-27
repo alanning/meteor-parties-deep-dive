79763d2d2060473a45eeb1cc79f89f9f6b2454e3
alanning <alanning@gmail.com>
2014-02-27 16:49:22 -0500

    step1 accounts allow/deny rules

--- model.js
+++ model.js
@@ -10,3 +10,26 @@
     title, description: String
 */
 Parties = new Meteor.Collection("parties");
+
+Parties.allow({
+  insert: function (userId, party) {
+    return userId && party.owner === userId;
+  },
+  update: function (userId, party, fields, modifier) {
+    if (userId !== party.owner)
+      return false; // not the owner
+
+    var allowed = ["title", "description", "x", "y"];
+    if (_.difference(fields, allowed).length)
+      return false; // tried to write to forbidden field
+
+    // A good improvement would be to validate the type of the new
+    // value of the field (and if a string, the length.) In the
+    // future Meteor will have a schema system to makes that easier.
+    return true;
+  },
+  remove: function (userId, party) {
+    // You can only remove parties that you created and nobody is going to.
+    return party.owner === userId && attending(party) === 0;
+  }
+});
